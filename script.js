8f0ef96a63b87b5d2c836dcce419cd0413547aea
Yasser Karami <yasser.karami@um5.ac.ma>
2024-12-19 22:19:01 +0100

    website

---
+++ script.js
@@ -0,0 +1,10 @@
+// Optional: Add some interactive features like a smooth scroll or other custom interactions
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
